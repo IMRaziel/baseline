e3a6d67f39de269857692cbfb9dff30cd1c37a29
raz <mir.raziel@gmail.com>
2020-02-05 14:33:55 +0300

    fixed double underscore  (__) when trying to make snake_case string from already snake cased string

--- src/Baseline/StringExtensions.cs
+++ src/Baseline/StringExtensions.cs
@@ -263,7 +263,7 @@ namespace Baseline
         /// <returns></returns>
         public static string SplitCamelCase(this string str)
         {
-            return Regex.Replace(Regex.Replace(str, @"(\P{Ll})(\P{Ll}\p{Ll})", "$1 $2"), @"(\p{Ll})(\P{Ll})", "$1 $2");
+            return Regex.Replace(Regex.Replace(str, @"(\P{Ll})(\P{Ll}\p{Ll})", "$1 $2"), @"(\p{Ll})(\P{Ll})", "$1 $2").Replace("_", "");
         }
 
         /// <summary>
